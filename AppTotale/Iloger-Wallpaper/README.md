9f91f1e3bba241d7c007542f70cb3018ba28cdfd
Salvatore <salvatore.foderaro@gmail.com>
2019-05-15 09:08:14 +0200

    App Totale

---
+++ AppTotale/Iloger-Wallpaper/README.md
@@ -0,0 +1,19 @@
+# Matrix-Rain-Live-Wallpaper
+Android matrix rain live wallpaper
+
+http://www.androidlearner.com/
+
+Create livewallpaper using my canvas matrix effect.
+Follow the tutorial at: 
+http://www.androidlearner.com/2017/01/android-live-wallpaper-tutorial.html
+
+
+<img src="https://github.com/sapandang/Matrix-Rain-Live-Wallpaper/blob/master/scr1.jpeg"  width="250"/>
+<img src="https://github.com/sapandang/Matrix-Rain-Live-Wallpaper/blob/master/scr2.jpeg"  width="250"/>
+<img src="https://github.com/sapandang/Matrix-Rain-Live-Wallpaper/blob/master/scr3.jpeg" width="250"/>
+
+Download apk  :-
+https://github.com/sapandang/Matrix-Rain-Live-Wallpaper/blob/master/app/build/outputs/apk/app-debug.apk
+
+Color picker library is by : enricocid
+https://github.com/enricocid/Color-picker-library
---
+++ AppTotale/Iloger-Wallpaper/app/src/main/java/it/iloger/wallpaper/LiveWallpaper.java
@@ -0,0 +1,34 @@
+package it.iloger.wallpaper;
+
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LiveWallpaper extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livewallpaper);
+
+        Intent intent = new Intent(this, RandomWallpaper.class);
+        startActivity(intent);
+    }
+
+    public void setWallpaper(View view) {
+        Intent intent = new Intent(
+                WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                new ComponentName(this, MatrixWallLiveWallpaper.class));
+        startActivity(intent);
+    }
+
+    public void wallpaperSettings(View view){
+        Intent intent = new Intent(this, MatrixWallSettings.class);
+        startActivity(intent);
+
+    }
+}
