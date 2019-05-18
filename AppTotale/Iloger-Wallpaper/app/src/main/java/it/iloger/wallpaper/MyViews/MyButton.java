89e5179b47ab9130750979ef833be76ff4ec24bd
Lorenzo <lorenzo.zara96@gmail.com>
2019-05-18 16:06:47 +0200

    App Totale V1.3
    Grafica Migliorata
    Stringhe tradotte

--- AppTotale/Iloger-Wallpaper/app/src/main/java/it/iloger/wallpaper/MyViews/MyButton.java
+++ AppTotale/Iloger-Wallpaper/app/src/main/java/it/iloger/wallpaper/MyViews/MyButton.java
@@ -5,15 +5,19 @@ import android.graphics.Color;
 import android.util.AttributeSet;
 import android.widget.Button;
 
+import it.iloger.wallpaper.R;
+
 public class MyButton extends android.support.v7.widget.AppCompatButton {
     public MyButton(Context context) {
         super(context);
-        this.setTextColor(Color.parseColor("BLUE"));
-        this.setTextColor(Color.parseColor("#FF0000"));
+        this.setTextColor(Color.parseColor("white"));
+        this.setBackgroundColor(getResources().getColor(R.color.buttonColor));
     }
 
     public MyButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.setTextColor(Color.parseColor("white"));
+        this.setBackgroundColor(getResources().getColor(R.color.buttonColor));
     }
 
     @Override
