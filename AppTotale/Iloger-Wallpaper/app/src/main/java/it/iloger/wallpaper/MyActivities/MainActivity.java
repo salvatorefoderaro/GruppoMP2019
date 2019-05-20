94cf445d18c505260f7f40b3471cf4c044636fae
Salvatore <salvatore.foderaro@gmail.com>
2019-05-20 14:55:09 +0200

    App Totale v1.3
    Aggiunto messaggio all'uscita

--- AppTotale/Iloger-Wallpaper/app/src/main/java/it/iloger/wallpaper/MyActivities/MainActivity.java
+++ AppTotale/Iloger-Wallpaper/app/src/main/java/it/iloger/wallpaper/MyActivities/MainActivity.java
@@ -1,8 +1,11 @@
 package it.iloger.wallpaper.MyActivities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,6 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         MyButton btt_camera = findViewById(R.id.btt_salvatore);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.alertBox));
+        builder.setTitle("Chiusura dell'applicazioene");
+        builder.setMessage("Sei sicuro di voler uscire dall'applicazione?");
+
+        // add the buttons
+        builder.setPositiveButton("Si, esci!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                System.exit(1);
+            }
+        });
+        builder.setNegativeButton("No!", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
 
