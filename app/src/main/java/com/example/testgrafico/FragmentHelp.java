e49103bbae57f69c5a6c754c861e1b2bcb8efaa6
Salvattò <a>
2019-06-26 09:37:09 +0200

    Ok

---
+++ app/src/main/java/com/example/testgrafico/FragmentHelp.java
@@ -0,0 +1,65 @@
+package com.example.testgrafico;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentHelp extends DialogFragment {
+
+    private Toolbar toolbar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_help_include, container);
+        toolbar = view.findViewById(R.id.toolbar1);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        toolbar.setTitle("Serve un aiuto?");
+
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_fragment_help, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+        menu.findItem(R.id.help).setVisible(false);
+        menu.findItem(R.id.save).setVisible(false);
+        menu.findItem(R.id.share).setVisible(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.close:
+                dismiss();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
