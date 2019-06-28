0098dd582c5fe40b62032183a79e982852f40fad
mattiapica <mattia_94p@htomail.it>
2019-06-28 11:19:47 +0200

    Add Max e Min

---
+++ app/src/main/java/com/example/testgrafico/MaxMin_Singleton.java
@@ -0,0 +1,30 @@
+package com.example.testgrafico;
+
+import com.example.testgrafico.MathHelper.getValueList;
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.ArrayList;
+
+public class MaxMin_Singleton {
+
+    private static MaxMin_Singleton instance = null;
+
+    public ArrayList<ArrayList<Entry>> getValues() {
+        return values;
+    }
+
+    public void setValues(ArrayList<ArrayList<Entry>> values) {
+        this.values = values;
+    }
+
+    private ArrayList<ArrayList<Entry>> values;
+
+    public static MaxMin_Singleton getInstance(){
+
+        if (instance == null){
+            instance = new MaxMin_Singleton();
+        }
+        return instance;
+
+    }
+}
